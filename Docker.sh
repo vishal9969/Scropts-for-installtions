fa8c58683aa7bef83cc7214fb13d26455636b575
vishal9969 <112000540+vishal9969@users.noreply.github.com>
2023-12-30 10:05:56 +0530

    Add files via upload

---
+++ Docker.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt install docker.io -y
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker ubuntu
+sudo systemctl restart docker
+sudo systemclt enable docker
+sudo chmod 777 /var/run/docker.sock
